a784ffba98dc13a85b03a0c5f466f3875309ee5f
Mario Amaya <thefozcat@gmail.com>
2019-04-24 13:51:08 -0700

    added post new user, updated status codes

--- users/users-router.js
+++ users/users-router.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     db.get()
         .then(users => {
-            res.json(users)
+            res.status(201).json(users)
         })
         .catch(err => {
             res.status(500).json({ error: err, message: 'Users information could not be retrieved.' })
@@ -23,10 +23,38 @@ router.get('/:id', (req, res) => {
 
     db.getById(userId)
         .then(user => {
-            res.json(user)
+            res.status(201).json(user)
         })
         .catch(err => {
             res.status(404).json({ error: err, message: 'The user with the specified ID does not exist' })
+        })
+})
+
+// GET user posts
+
+router.get('/:id/posts', (req, res) => {
+    const userId = req.params.id;
+
+    db.getUserPosts(userId)
+        .then(posts => {
+            res.status(201).json(posts)
+        })
+        .catch(err => {
+            res.status(404).json({ error: err, message: 'Cannot retrieve posts.' })
+        })
+})
+
+// POST (add new user)
+
+router.post('/', (req, res) => {
+    const newUser = req.body;
+
+    db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'There was an error adding the user to the database.' })
         })
 })
 
