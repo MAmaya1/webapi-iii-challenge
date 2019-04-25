5328053047d4b4a5704de317a833485f74fb966b
Mario Amaya <thefozcat@gmail.com>
2019-04-25 12:12:38 -0700

    created dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // code away!
 const server = require('./server');
 
-server.listen(5000, () => {
+const port = process.env.PORT || 5000; // for dynamic port
+
+server.listen(port, () => {
     console.log('\n *** Server running on http://localhost:5000 *** \n')
 });
