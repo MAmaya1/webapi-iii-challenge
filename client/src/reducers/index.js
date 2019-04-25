27713ec2fcf931ab16d173223e79de40a89b0659
Mario Amaya <thefozcat@gmail.com>
2019-04-24 17:37:03 -0700

    updated reducer - get users

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -10,11 +10,34 @@ import {
 
 const initialState = {
     users: [],
-    posts: []
+    posts: [],
+    loadingUsers: false,
+    loadingUsersErr: null
 }
 
 function reducer(state = initialState, action) {
     switch (action.type) {
+        case GET_USERS_START:
+            return {
+                ...state,
+                loadingUsers: true,
+                loadingUsersErr: null
+            }
+
+        case GET_USERS_SUCCESS:
+            return {
+                ...state,
+                loadingUsers: false,
+                loadingUsersErr: null,
+                users: action.payload
+            }
+
+        case GET_USERS_FAILURE:
+            return {
+                ...state,
+                loadingUsers: false,
+                loadingUsersErr: action.payload
+            }
         default: 
             return state;
     }
